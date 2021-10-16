3ec1694e0d1d3a91278031fc819506e7beb5e5c1
Manali Biswas <46257679+manali-biswas@users.noreply.github.com>
2021-10-16 15:39:54 +0530

    changed to node.js env and added file upload to azure

--- public/js/script2.js
+++ public/js/script2.js
@@ -18,7 +18,8 @@ input.addEventListener("change", function(){
 
 
 //If user Drag File Over DropArea
-dropArea.addEventListener("dragover", (event)=>{
+dropArea.addEventListener("dragover", (event) => {
+  event.stopPropagation();
   event.preventDefault(); //preventing from default behaviour
   dropArea.classList.add("active");
   dragText.textContent = "Release to Upload File";
@@ -31,28 +32,32 @@ dropArea.addEventListener("dragleave", ()=>{
 });
 
 //If user drop File on DropArea
-dropArea.addEventListener("drop", (event)=>{
+dropArea.addEventListener("drop", (event) => {
+  event.stopPropagation();
   event.preventDefault(); //preventing from default behaviour
   //getting user select file and [0] this means if user select multiple files then we'll select only the first one
   file = event.dataTransfer.files[0];
   showFile(); //calling function
 });
 
-function showFile(){
+async function showFile(){
   let fileType = file.type; //getting selected file type
-  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  let validExtensions = ["image/png","video/avi", "video/mp4"]; //adding some valid image extensions in array
   if(validExtensions.includes(fileType)){ //if user selected file is an image file
     let fileReader = new FileReader(); //creating new FileReader object
-    fileReader.onload = ()=>{
+    fileReader.onload = async ()=>{
       let fileURL = fileReader.result; //passing user file source in fileURL variable
+      
         // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
-      // let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
-      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+       /*let vidTag = `<video controls id='video-tag'><source src=${fileURL}></video>`; //creating an img tag and passing user selected file source inside src attribute
+      dropArea.innerHTML = vidTag; //adding that created vid tag inside dropArea container
+      const videoTag = document.querySelector("#video-tag");
+      videoTag.load();*/ //No need to load on client side as well
     }
     fileReader.readAsDataURL(file);
-  }//else{
-   // alert("This is not an Image File!");
-  //  dropArea.classList.remove("active");
-    //dragText.textContent = "Drag & Drop to Upload File";
- // }
+  }else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
 }
