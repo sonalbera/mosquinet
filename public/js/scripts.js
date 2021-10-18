7a5e6cabf1c363597e6c8ee34ec0febda86e3d2d
Sonal Bera <sonalbera_2k18it120@dtu.ac.in>
2021-10-18 19:32:02 +0530

    fixed https

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,3 @@
-/*
-	Visual by TEMPLATE STOCK
-	templatestock.co @templatestock
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
 
 (function($) {
     "use strict";
